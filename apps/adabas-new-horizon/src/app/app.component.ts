670d884e695526786c38973e5db43c99eb04c36b
Emily,Tan (ETA) <Emily.Tan@softwareag.com>
2019-10-21 16:47:19 +0800

    material UI library added

--- apps/adabas-new-horizon/src/app/app.component.ts
+++ apps/adabas-new-horizon/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@ada-new-horizon/api-interfaces';
+import { MDCDataTableRowSelectionChangedEvent } from '@angular-mdc/web/data-table';
 
 @Component({
   selector: 'ada-new-horizon-root',
@@ -9,5 +10,26 @@ import { Message } from '@ada-new-horizon/api-interfaces';
 })
 export class AppComponent {
   hello$ = this.http.get<Message>('/api/hello');
+  selectionChangedEvent: any;
+
   constructor(private http: HttpClient) {}
+
+  desserts = [
+    {checked: false, name: 'Frozen yogurt', calories: 159, carbs: 24, protein: 4, comment: 'Super tasty'},
+    {checked: true, name: 'Ice cream sandwich', calories: 237, carbs: 37, protein: 4.3, comment: 'I like ice cream more'},
+    {checked: false, name: 'Eclair', calories: 262, carbs: 16, protein: 6, comment: 'New filling flavor'}
+  ];
+  
+  onSelectionChanged(event: MDCDataTableRowSelectionChangedEvent): void {
+    this.selectionChangedEvent = event;
+    this.desserts[event.index].checked = event.selected;
+  }
+  
+  onSelectedAll(): void {
+    this.desserts.forEach(_ => _.checked = true);
+  }
+  
+  onUnselectedAll(): void {
+    this.desserts.forEach(_ => _.checked = false);
+  }
 }
--- apps/adabas-new-horizon/src/app/app.module.ts
+++ apps/adabas-new-horizon/src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  MdcButtonModule,
+  MdcFabModule,
+  MdcIconModule,
+  MDCDataTableModule,
+  MdcCheckboxModule, 
+} from '@angular-mdc/web';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    MdcButtonModule,
+    MdcFabModule,
+    MdcIconModule,
+    MDCDataTableModule, 
+    MdcCheckboxModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
+
