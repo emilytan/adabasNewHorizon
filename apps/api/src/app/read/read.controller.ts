12940e18f5ee6a58de87baa3533e10485f1ada5e
Chee Guan, Ang(CHEA) <cheeguan.ang@softwareag.com>
2019-10-21 14:17:36 +0800

    NestJS, updated read controller

--- apps/api/src/app/read/read.controller.ts
+++ apps/api/src/app/read/read.controller.ts
@@ -1,12 +1,51 @@
-import { Controller } from '@nestjs/common';
-import { Get } from '@nestjs/common';
+import { Controller, Get, Query, Param, Body } from '@nestjs/common';
+import { Adabas, AdabasMap } from 'adabas-tcp';
+import { object } from '@hapi/joi';
 
-@Controller('read')
+@Controller(':host/:port/read')
 export class ReadController {
-    @Get()
-    async getAll(): Promise<any> {
+    @Get('fileid/:fileid')
+    async getAll(@Param('fileid') fileid, @Body() body, @Param('host') host, @Param('port') port): Promise<any> {
         try {
-           return 'test read';
+            // const Employee = new AdabasMap(11)
+            //     .alpha(8, 'AA', { name: 'Personnel ID' })
+            //     .alpha(3, 'AZ', { name: 'Language', occ: 3 });
+            // console.log(Employee);
+            const ada = new Adabas(host, port);
+            return ada.read({ fnr: fileid, map: body.map || null }).then(res => {
+                // console.log(res);
+                ada.close().then(() => ada.disconnect());
+                return res;
+            });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+    @Get('fileid/:fileid/isn/:isnid')
+    async getbyISN(@Param('fileid') fileid, @Param('isnid') isnid, @Body() body, @Param('host') host, @Param('port') port): Promise<any> {
+        try {
+            const ada = new Adabas(host, port);
+            return ada.read({ fnr: fileid, map: body.map || null, isn: isnid }).then(res => {
+                // console.log(res);
+                ada.close().then(() => ada.disconnect());
+                return res;
+            });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+    @Get('fileid/:fileid/criteria')
+    async getbyCriteria(@Param('fileid') fileid, @Body() body, @Query() params,
+        @Param('host') host, @Param('port') port): Promise<any> {
+        try {
+            const ada = new Adabas(host, port);
+            return ada.read({ fnr: fileid, map: body.map || null, criteria: body.criteria }).then(res => {
+                // console.log(res);
+                ada.close().then(() => ada.disconnect());
+                return res;
+            });
         } catch (error) {
             console.log(error);
             throw error;
