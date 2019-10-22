1cf99b584d217dc8fe46d1041ef3abfb498a8903
Choong Hoe, Chow <CC>
2019-10-22 18:24:38 +0800

    Display sample result in table including object and arrays toString()

--- apps/adabas-new-horizon/src/app/adabas-query/read-result/read-result.component.ts
+++ apps/adabas-new-horizon/src/app/adabas-query/read-result/read-result.component.ts
@@ -7,16 +7,24 @@ import { sampleResult } from './sample-result';
   styleUrls: ['./read-result.component.scss']
 })
 export class ReadResultComponent implements OnInit {
-
   keys;
   result = sampleResult;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     console.log(this.result);
     console.log('keys', Object.keys(this.result[0]));
 
-    this.keys = Object.keys(this.result[0]);
+    if (this.result.length) {
+      this.keys = Object.keys(this.result[0]);
+      this.result.forEach((row) => {
+        this.keys.forEach(element => {
+          if (Array.isArray(row[element]) || (row[element] !== null && typeof (row[element]) === 'object')) {
+            row[element] = JSON.stringify(row[element]);
+          }
+          
+        });
+      });
+    }
   }
-
 }
