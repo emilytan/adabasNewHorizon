259252457807bc9deb577c53c5aca26609735ec8
Chee Guan, Ang(CHEA) <cheeguan.ang@softwareag.com>
2019-10-24 10:34:54 +0800

    NestJS, added fdt controller and sql controller

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { CreateController } from './create/create.controller';
 import { UpdateController } from './update/update.controller';
 import { DeleteController } from './delete/delete.controller';
 import { FileIoController } from './file-io/file-io.controller';
+import { SqlController } from './sql/sql.controller';
+import { FdtController } from './fdt/fdt.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ReadController, CreateController, UpdateController, DeleteController, FileIoController],
+  controllers: [AppController, ReadController, CreateController, UpdateController, DeleteController, FileIoController, SqlController, FdtController],
   providers: [AppService]
 })
 export class AppModule {}
